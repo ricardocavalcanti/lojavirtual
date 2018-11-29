9c2dc02562db17f1e64ba5ba11abb9e6d77da783
ricardo <ricardo@RainDuck>
2018-11-28 22:15:53 -0300

    Inserindo dados no banco

--- lojavirtual/src/main/java/br/com/lojavirtual/beans/AdminLivrosBeans.java
+++ lojavirtual/src/main/java/br/com/lojavirtual/beans/AdminLivrosBeans.java
@@ -1,8 +1,11 @@
 package br.com.lojavirtual.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
+import br.com.lojavirtual.daos.LivroDao;
 import br.com.lojavirtual.models.Livro;
 
 // CDI
@@ -12,9 +15,14 @@ public class AdminLivrosBeans {
 	
 	private Livro livro = new Livro();
 	
+	//Context and dependency injection
+	@Inject
+	private LivroDao dao;
 	
+	@Transactional
 	public void salvar() {
 		
+		dao.salvar(livro);
 		System.out.println("Livro cadastrado: "+livro);		
 	}
 
